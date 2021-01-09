c6681619ff1ad0b096b93390afd2f1ce0d4991d9
Arda Akdemir <ardaakdemir1@gmail.com>
2021-01-09 16:27:12 +0900

    typo

--- test/test_tokenizer.py
+++ test/test_tokenizer.py
@@ -14,31 +14,26 @@ def parse_args():
         help="The path to load the model to continue training."
     )
 
-
     args = parser.parse_args()
     args.device = device
     return args
 
 
-sent_dict = {"tr":"Sen benim kim olduğumu biliyor musun?",
-             "cs":"Potřebujete rychle poradit?",
-             "hu":"Az ezredfordulós szilveszter valószínűleg az átlagos év véginél komolyabb feladatokat ró a sürgősségi betegellátás szervezeteire és a rendőrségre.",
-             "jp":"為せば成る為さねば成らぬ。\n麩菓子は、麩を主材料とした日本の菓子。",
-             "fi":" Showroomilla esiteltiin uusi The Garden Collection ja tarjoiltiin maukasta aamupalaa aamu-unisille muotibloggaajille."}
+sent_dict = {"tr": "Sen benim kim olduğumu biliyor musun?",
+             "cs": "Potřebujete rychle poradit?",
+             "hu": "Az ezredfordulós szilveszter valószínűleg az átlagos év véginél komolyabb feladatokat ró a sürgősségi betegellátás szervezeteire és a rendőrségre.",
+             "jp": "為せば成る為さねば成らぬ。\n麩菓子は、麩を主材料とした日本の菓子。",
+             "fi": " Showroomilla esiteltiin uusi The Garden Collection ja tarjoiltiin maukasta aamupalaa aamu-unisille muotibloggaajille."}
 
-
-
-    "Sen benim kim olduğumu biliyor musun?\nThe more you use it the more you need it."
-
-
-
-model_name_dict = {"jp":"cl-tohoku/bert-base-japanese",
-                   "tr":"dbmdz/bert-base-turkish-cased",
-                   "hu":"SZTAKI-HLT/hubert-base-cc",
-                   "fi":"TurkuNLP/bert-base-finnish-cased-v1 ",
-                   "cs":"DeepPavlov/bert-base-bg-cs-pl-ru-cased"}
+model_name_dict = {"jp": "cl-tohoku/bert-base-japanese",
+                   "tr": "dbmdz/bert-base-turkish-cased",
+                   "hu": "SZTAKI-HLT/hubert-base-cc",
+                   "fi": "TurkuNLP/bert-base-finnish-cased-v1 ",
+                   "cs": "DeepPavlov/bert-base-bg-cs-pl-ru-cased"}
 
 output_file = "tokenized.txt"
+
+
 def test_tokenizers():
     for lang in model_name_dict:
         sent = sent_dict[lang]
@@ -48,7 +43,13 @@ def test_tokenizers():
         print(model)
         with open(output_file, "a", encoding="utf-8") as o:
             a.write("{}\n".format(lang))
-            a.write("Sentence: "+sent)
+            a.write("Sentence: " + sent)
             o.write("\n")
-            o.write("Tokens: "+" ".join(tokens))
+            o.write("Tokens: " + " ".join(tokens))
             o.write("\n")
+
+def main():
+    test_tokenizers()
+
+if __name__ == "__main__":
+    main()
