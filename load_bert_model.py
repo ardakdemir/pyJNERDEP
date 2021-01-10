7ba1e2cb9583cc51860cc690968ee8fd13fb5a18
Arda Akdemir <ardaakdemir1@gmail.com>
2021-01-10 15:09:35 +0900

    test

---
+++ load_bert_model.py
@@ -0,0 +1,40 @@
+from transformers import BertModel, AutoTokenizer, AutoModel, BertForPreTraining, BertForTokenClassification, \
+    AutoModelForTokenClassification
+import torch.nn as nn
+import torch
+
+model_name_dict = {"jp": "cl-tohoku/bert-base-japanese",
+                   "tr": "dbmdz/bert-base-turkish-cased",
+                   "hu": "hubert",
+                   "fi": "TurkuNLP/bert-base-finnish-cased-v1",
+                   "cs": "DeepPavlov/bert-base-bg-cs-pl-ru-cased"}
+
+
+def load_bert_model(lang):
+    model_name = model_name_dict[lang]
+    if lang == "hu":
+        model = BertForPreTraining.from_pretrained(model_name, from_tf=True,output_hidden_states=True)
+    else:
+        model = BertForTokenClassification.from_pretrained(model_name)
+    return model
+
+
+lang = "hu"
+model = load_bert_model(lang)
+
+# model.cls = nn.Identity()
+# model = nn.Sequential(*list(model.children())[:-1])
+tokenizer = AutoTokenizer.from_pretrained("dbmdz/bert-base-turkish-cased")
+input = tokenizer.tokenize("Arda Akdemir  nereye gidiyorsunuz.")
+input = torch.LongTensor(tokenizer.convert_tokens_to_ids(input)).reshape(1, -1)
+print(input.shape)
+# bert_token_ids = torch.randint(0, 10, (3,12),dtype=torch.Long)
+# print(bert_token_ids.shape)
+attention_mask = torch.ones(*input.shape)
+if lang == "hu":
+    output = model(input, attention_mask)[2][-1]
+else:
+    output = model(input, attention_mask)[0]
+
+print(output.shape)
+assert output.shape == (input.shape[0],input.shape[1],768)
---
+++ test/test_models.py
@@ -0,0 +1,91 @@
+from transformers import AutoTokenizer, AutoModel,BertForPreTraining
+
+import argparse
+
+
+sent_dict = {"tr": "Sen benim kim olduğumu biliyor musun?",
+             "cs": "Potřebujete rychle poradit?",
+             "hu": "Az ezredfordulós szilveszter valószínűleg az átlagos év véginél komolyabb feladatokat ró a sürgősségi betegellátás szervezeteire és a rendőrségre.",
+             "jp": "為せば成る為さねば成らぬ。\n麩菓子は、麩を主材料とした日本の菓子。",
+             "fi": " Showroomilla esiteltiin uusi The Garden Collection ja tarjoiltiin maukasta aamupalaa aamu-unisille muotibloggaajille."}
+
+model_name_dict = {"jp": "cl-tohoku/bert-base-japanese",
+                   "tr": "dbmdz/bert-base-turkish-cased",
+                   "hu": "/home/aakdemir/bert_models/hubert",
+                   "fi": "TurkuNLP/bert-base-finnish-cased-v1",
+                   "cs": "DeepPavlov/bert-base-bg-cs-pl-ru-cased"}
+
+output_file = "tokenized.txt"
+
+
+
+class BertModelforJoint(nn.Module):
+
+    def __init__(self, lang):
+        super(JointModel, self).__init__()
+        self.model = self.load_bert_model(lang)
+        self.lang = lang
+        # base model for generating bert output
+        self.base_model = BaseModel(self.args, tokenizer)
+
+        self.args['lstm_input_size'] = self.base_model.lstm_input_size
+
+    def load_bert_model(self,lang):
+        model_name = model_name_dict[lang]
+        if lang == "hu":
+            model = BertForPreTraining.from_pretrained(model_name, from_tf=True, output_hidden_states=True)
+        else:
+            model = BertForTokenClassification.from_pretrained(model_name)
+            model.classifier = nn.Identity()
+        return model
+
+    def forward(self,input,attention_mask,**kwargs):
+        """
+            Output the logits of the last layer for each word...
+        :param input:
+        :return:
+        """
+        if self.lang == "hu":
+            output = model(input, attention_mask)[2][-1]
+        else:
+            output = model(input, attention_mask)[0]
+        return output
+
+def load_bert_model(lang):
+    model_name = model_name_dict[lang]
+    if lang == "hu":
+        model = BertForPreTraining.from_pretrained(model_name, from_tf=True,output_hidden_states=True)
+    else:
+        model = BertForTokenClassification.from_pretrained(model_name)
+        model.classifier = nn.Identity()
+    return model
+
+
+def test_models():
+    for lang,model_name in model_name_dict.items():
+        sent = sent_dict[lang]
+        print("Testing {}...".format(lang))
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = BertModelforJoint(lang)
+        tokens = tokenizer.tokenize(sent)
+        input = torch.LongTensor(tokenizer.convert_tokens_to_ids(input)).reshape(1, -1)
+        print("Token ids: {}".format(input))
+        attention_mask = torch.ones(*input.shape)
+        if lang == "hu":
+            output = model(input, attention_mask)[2][-1]
+        else:
+            output = model(input, attention_mask)[0]
+        assert output.shape == (*input.shape,768)
+        # print(model)
+        with open(output_file, "a", encoding="utf-8") as o:
+            o.write("{}\n".format(lang))
+            o.write("Sentence: " + sent)
+            o.write("\n")
+            o.write("Tokens: " + " ".join(tokens))
+            o.write("\n")
+
+def main():
+    test_models()
+
+if __name__ == "__main__":
+    main()
