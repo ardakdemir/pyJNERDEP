865afedc3c9235c770d54ca86529a20d733cf2a4
Arda Akdemir <ardaakdemir1@gmail.com>
2021-01-12 00:04:22 +0900

    test for czech

--- jointtrainer_multilang.py
+++ jointtrainer_multilang.py
@@ -79,17 +79,17 @@ encoding_map = {"cs": "latin-1",
                 "hu": "utf-8",
                 "fi": "utf-8"}
 
-word2vec_dict = {"jp": "word_vecs/jp/jp.bin",
-                 "tr": "word_vecs/tr/tr.bin",
-                 "hu": "word_vecs/hu/hu.bin",
-                 "fi": "word_vecs/fi/fi.bin",
-                 "cs": "word_vecs/cs/cs.txt"}
+word2vec_dict = {"jp": "../word_vecs/jp/jp.bin",
+                 "tr": "../word_vecs/tr/tr.bin",
+                 "hu": "../word_vecs/hu/hu.bin",
+                 "fi": "../word_vecs/fi/fi.bin",
+                 "cs": "../word_vecs/cs/cs.txt"}
 
-fasttext_dict = {"jp": "word_vecs/jp/cc.jp.300.bin",
-                 "tr": "word_vecs/tr/cc.tr.300.bin",
-                 "hu": "word_vecs/hu/cc.hu.300.bin",
-                 "fi": "word_vecs/fi/cc.fi.300.bin",
-                 "cs": "word_vecs/cs/cc.cs.300.bin"}
+fasttext_dict = {"jp": "../word_vecs/jp/cc.jp.300.bin",
+                 "tr": "../word_vecs/tr/cc.tr.300.bin",
+                 "hu": "../word_vecs/hu/cc.hu.300.bin",
+                 "fi": "../word_vecs/fi/cc.fi.300.bin",
+                 "cs": "../word_vecs/cs/cc.cs.300.bin"}
 
 word2vec_lens = {"tr": 200,
                  "hu": 300,
