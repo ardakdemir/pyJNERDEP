b8818031796b8f7eaf16bf8ac78431a23eaf714b
Arda Akdemir <ardaakdemir1@gmail.com>
2021-01-12 15:34:25 +0900

    test for czech

--- test/test_readers.py
+++ test/test_readers.py
@@ -26,33 +26,35 @@ batch_size = 300
 data_folder = "/home/aakdemir/datasets"
 
 # config
-for lang in model_name_dict.keys():
-    print("\nTrying to read {} datasets".format(lang))
-    for task in ["NER", "DEP"]:
-        print("Reading {} dataset".format(task))
-        encoding = encoding_map[lang]
-        model_name = model_name_dict[lang]
-        language = lang_abs[lang]
+def test_readers():
+    for lang in model_name_dict.keys():
+        print("\nTrying to read {} datasets".format(lang))
+        for task in ["NER", "DEP"]:
+            print("Reading {} dataset".format(task))
+            encoding = encoding_map[lang]
+            model_name = model_name_dict[lang]
+            language = lang_abs[lang]
 
-        if task == "NER":
-            file_name = os.path.join(data_folder, "myner_{}-train.txt".format(language))
-        else:
-            file_name = os.path.join(data_folder, "dep_{}_train.conllu".format(language))
+            if task == "NER":
+                file_name = os.path.join(data_folder, "myner_{}-train.txt".format(language))
+            else:
+                file_name = os.path.join(data_folder, "dep_{}_train.conllu".format(language))
 
-        bert_tokenizer = AutoTokenizer.from_pretrained(model_name)
-        bert_tokenizer.add_tokens(['[SOS]', '[EOS]', '[ROOT]', '[PAD]'])
-        if task == "NER":
-            data_reader = DataReader(file_name, task, batch_size=batch_size,
-                                     tokenizer=bert_tokenizer)
-        elif task == "DEP":
-            data_reader = DepDataset(file_name, batch_size=12,
-                                     tokenizer=bert_tokenizer)
-        batch = data_reader[0]
-        for x in batch:
-            logging.info(x)
-            logging.info("\n")
-        l = len(data_reader)
+            bert_tokenizer = AutoTokenizer.from_pretrained(model_name)
+            bert_tokenizer.add_tokens(['[SOS]', '[EOS]', '[ROOT]', '[PAD]'])
+            if task == "NER":
+                data_reader = DataReader(file_name, task, batch_size=batch_size,
+                                         tokenizer=bert_tokenizer)
+            elif task == "DEP":
+                data_reader = DepDataset(file_name, batch_size=12,
+                                         tokenizer=bert_tokenizer)
+            batch = data_reader[0]
+            for x in batch:
+                logging.info(x)
+                logging.info("\n")
+            l = len(data_reader)
 
-        print("Read {} batches ".format(l))
-        return
-    
+            print("Read {} batches ".format(l))
+            return
+
+test_readers()
