588b3da058caf33eb50d4f9fdfd99967cf960fb9
Arda Akdemir <ardaakdemir1@gmail.com>
2021-01-14 15:50:01 +0900

    test for czech

--- test/test_jointmodel.py
+++ test/test_jointmodel.py
@@ -4,3 +4,8 @@ from jointtrainer_multilang import JointModel, parse_args, JointTrainer
 args = parse_args()
 joint_trainer = JointTrainer(args)
 joint_trainer.init_models()
+
+state_dict = joint_trainer.jointmodel.state_dict()
+
+print("My state dictionary")
+print(state_dict)
