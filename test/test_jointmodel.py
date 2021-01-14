1f168fe7331ea618fc26e82024bf0183c8167c52
Arda Akdemir <ardaakdemir1@gmail.com>
2021-01-14 17:37:04 +0900

    test for czech

--- test/test_jointmodel.py
+++ test/test_jointmodel.py
@@ -1,5 +1,5 @@
 from jointtrainer_multilang import JointModel, parse_args, JointTrainer
-
+import torch
 
 args = parse_args()
 joint_trainer = JointTrainer(args)
@@ -7,5 +7,14 @@ joint_trainer.init_models()
 
 crf_weights = joint_trainer.jointmodel.nermodel.crf.emission.weight
 
-print("My crf weights")
+
+print("My crf weights before")
+print(crf_weights)
+
+
+load_path = args["load_path"]
+joint_trainer.jointmodel.load_state_dict(torch.load(load_path)
+                                         
+crf_weights = joint_trainer.jointmodel.nermodel.crf.emission.weight
+print("My crf weights after")
 print(crf_weights)
