9276e53c026995154cf3a0849a0e402a4b16a461
Arda Akdemir <ardaakdemir1@gmail.com>
2021-01-19 22:53:44 +0900

    test for czech

--- vocab.py
+++ vocab.py
@@ -9,7 +9,6 @@ class Vocab:
         self.w2ind = w2ind
         self.ind2w = [x for x in w2ind.keys()]
         self.unk_ind = unk_ind
-        self.unk = unk
 
     def map(self, units):
         return [self.w2ind.get(x, self.unk_ind) for x in units]
