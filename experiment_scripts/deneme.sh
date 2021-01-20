76d4fde46036089b1ed6739552d05d16697e62a8
Arda Akdemir <ardaakdemir1@gmail.com>
2021-01-20 17:07:34 +0900

    test for czech

--- experiment_scripts/deneme.sh
+++ experiment_scripts/deneme.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-file_path="../../datasets/"${lang}"_"${domain}"_train.json"
-if [ -f ${file_path} ]
-then
-  echo $file_path
-fi
+
+langs=(czech hungarian japanese english finnish  turkish)
+word_vec_prefs=("cs" "hu" "jp" "en" "fi" "tr")
+domains=("movie" "twitter")
+
+for i in 0 1 2 3 4 5
+do
+  lang=${langs[$i]}
+  for j in 0 1
+  do
+    domain=${domains[$j]}
+    file_path="../../datasets/"${lang}"_"${domain}"_train.json"
+    if [ -f ${file_path} ]
+    then
+      echo $file_path" "${lang}" "${domain}
+    fi
+  done
+done
