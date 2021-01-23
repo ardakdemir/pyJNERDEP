4b9798e88ba72d4622886fae6996805aa3a78102
Arda Akdemir <ardaakdemir1@gmail.com>
2021-01-23 21:30:31 +0900

    test for czech

--- experiment_scripts/sentiment_analysis_singlesubmit.sh
+++ experiment_scripts/sentiment_analysis_singlesubmit.sh
@@ -17,5 +17,5 @@ dev_path="../../datasets/sa_"${domain}"_"${lang}"-dev.json"
 test_path="../../datasets/sa_"${domain}"_"${lang}"-test.json"
 if [ -f ${train_path} ]
 then
-singularity exec --nv  --writable ~/singularity/pt-cuda-tf-tr-ft python sequence_trainer.py  --domain ${domain} --sa_train_file ${train_path} --sa_dev_file ${dev_path} --sa_test_file ${test_path}  --exp_file ${exp_file} --word_embed_type ${type} --lang $l --batch_size ${batch_size} --epochs 10 --save_folder $load_dir --eval_interval ${eval_interval}
+singularity exec --nv  --writable ~/singularity/pt-cuda-tf-tr-ft python sequence_trainer.py  --domain ${domain} --sa_train_file ${train_path} --sa_dev_file ${dev_path} --sa_test_file ${test_path}   --word_embed_type ${type} --lang $l --batch_size ${batch_size} --epochs 10 --save_folder $load_dir --eval_interval ${eval_interval}
 fi
