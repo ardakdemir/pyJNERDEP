1bf79dd7443f2fac81b46da9547ca3031bfd107c
Arda Akdemir <ardaakdemir1@gmail.com>
2021-03-30 15:17:24 +0900

    update requirements

--- experiment_scripts/run_sa_models.sh
+++ experiment_scripts/run_sa_models.sh
@@ -7,8 +7,9 @@ lang_prefs=("tr" "en")
 lang_pref=${1} #tr or en
 lang=${2}
 domain=${3} #movie or twitter
-model_folder=${4}
-save_dir=${5}
+word_type=${4}
+model_folder=${5}
+save_dir=${6}
 
 model_type="sa"
 exp_key=${domain}"_"${lang}
@@ -18,5 +19,5 @@ python download_storedmodels.py --model_type SA --word_type ${exp_key} --save_fo
 # for type in bert mbert bert_en fastext word2vec random_init
 # for type in bert mbert bert_en fastext word2vec random_init
 #for type in bert mbert bert_en fastext word2vec random_init
-echo "Running for " ${model_type}"  "${lang}"  "${type}
-python sequence_trainer.py --mode predict  --word_embed_type ${type}  --lang ${lang_pref}  --save_folder $save_dir --load_folder ${model_folder}
+echo "Running for " ${model_type}"  "${lang}"  "${word_type}
+python sequence_trainer.py --mode predict  --word_embed_type ${word_type}  --lang ${lang_pref}  --save_folder $save_dir --load_folder ${model_folder}
