8eeb01fb6ba1d36712ad3fd3d4b63e2a3eb1b2c0
Arda Akdemir <ardaakdemir1@gmail.com>
2021-03-31 14:12:30 +0900

    update requirements

---
+++ experiment_scripts/run_flat_models.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+model_folder=${1}
+task_name=${2}
+save_folder=${3}
+
+langs=(czech turkish  japanese english finnish hungarian)
+lang_prefs=("cs" "tr"  "jp" "en" "fi" "hu")
+model_name="FLAT"
+
+
+for i in 0 1 2 3 4 5
+do
+  lang=${langs[$i]}
+  lang_pref=${lang_prefs[$i]}
+#    for type in bert mbert bert_en fastext word2vec random_init
+  for type in fastext word2vec
+  do
+    echo "Running for " ${model_type}"  "${lang}"  "${type}
+    #load_path = os.path.join(model_folder,"{}_{}_{}_best_{}_model.pkh".format(task,word_type,l_p,task_lower))
+    lower_task="$(echo $task_name | tr '[A-Z]' '[a-z]')"
+    load_path=$model_folder"/"${model_name}"_"${type}"_"${lang_pref}"_best_"${lower_task}"_model.pkh"
+    echo "Loading model from: "${load_path}
+    python jointtrainer_multilang.py --model_type ${model_name} --eval_mode ${task_name} --mode "predict" --word_embed_type ${type}   --lang ${lang_pref}  --save_dir $save_dir --load_model 1 --load_path $load_path
+  done
+done
